0d79dd0aa897d38189298458a4308f82d3bbc4ae
tester <gittester77@gmail.com>
2024-11-14 07:19:28 +0530

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Button from './Button'
+
+function App() {
+
+  return (
+    <>
+      <div>Welcome to my app</div>
+      <Button title='Press me🍑 🍑 '/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Button.tsx
@@ -0,0 +1,8 @@
+function Button({title}:{title:string}) {
+
+  return (
+    <button style={{cursor:'pointer'}}>{title}</button>
+  )
+}
+
+export default Button
